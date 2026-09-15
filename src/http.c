42b5695499b6d9557a6e4fc2950e10c2d4f2e24d
Andrew Shershunov <the.evilur@gmail.com>
2026-09-15 20:29:45 +0300

    feat: Add simple http api

--- src/http.c
+++ src/http.c
@@ -1,5 +1,158 @@
 #include "http.h"
+#include "error.h"
+
+#include <stdio.h>
+#include <string.h>
 
 int32_t http_handle_request(char* buffer, int32_t size) {
     return *buffer + size;
+}
+
+int32_t http_send_default_response(const socket_fd_t socket_fd,
+                                   char* const buffer, int32_t buffer_size,
+                                   const int32_t status_code,
+                                   const char* status_description) {
+    /* Default response */
+    static const char response_headers[] =
+        "HTTP/1.1 %d %s\r\n"
+        "Content-Length: %d\r\n"
+        "Connection: close\r\n"
+        "\r\n";
+    static const char response_body[] =
+        "<!DOCTYPE html><html>"
+        "<head><title>%d %s</title></head>"
+        "<body><h1 style='text-align:center'>%d %s</h1><hr></body>"
+        "</html>";
+
+    /* Calculate the body size */
+    const int32_t calced_body_size = (int32_t)sizeof(response_body)
+        -1                                          // \0 char
+        -4 * 2                                      // %d %s (twice)
+        +3 * 2                                      // status code (twice)
+        + (int32_t)strlen(status_description) * 2;  // Description (twice)
+
+    /* Pointer to the buffer with offset */
+    char* buffer_ptr = buffer;
+
+    /* Write the headers to the buffer */
+    const int32_t headers_size = snprintf(buffer_ptr, (uint64_t)buffer_size,
+                                          response_headers,
+                                          status_code, status_description,
+                                          calced_body_size);
+    if (headers_size >= buffer_size ||
+        headers_size < 0) {
+        printerr("Failed to send the default response:\n"
+                 "Buffer is too small");
+        return -1;
+    }
+    buffer_ptr += headers_size;
+    buffer_size -= headers_size;
+
+    /* Write the body to the buffer */
+    const int32_t body_size = snprintf(buffer_ptr, (uint64_t)buffer_size,
+                                       response_body,
+                                       status_code, status_description,
+                                       status_code, status_description);
+    if (body_size >= buffer_size ||
+        body_size < 0) {
+        printerr("Failed to send the default response:\n"
+                 "Buffer is too small");
+        return -1;
+    }
+
+    /* Check if out body size calulations were right */
+    if (body_size != calced_body_size) {
+        printerr("Failed to calculate the body size");
+        return -1;
+    }
+
+    /* Write the response to the buffer */
+    socket_write(socket_fd, buffer, headers_size + body_size);
+
+    /* Return the success code */
+    return 0;
+}
+
+http_request_method_t http_get_request_method(
+    const http_request_headers_t* const request_headers
+) {
+    return request_headers->flags & HTTP_METHOD_MASK;
+}
+
+void http_set_request_method(
+    http_request_headers_t* const request_headers,
+    const http_request_method_t request_method
+) {
+    request_headers->flags =
+        (request_headers->flags & ~HTTP_METHOD_MASK) |
+        (request_method & HTTP_METHOD_MASK);
+}
+
+http_transfer_encoding_t http_get_transfer_encoding(
+    const http_request_headers_t* const request_headers
+) {
+    return request_headers->flags & HTTP_TRANSFER_ENCODING_MASK;
+}
+
+void http_set_transfer_encoding(
+    http_request_headers_t* const request_headers,
+    const http_transfer_encoding_t transfer_encoding
+) {
+    request_headers->flags =
+        (request_headers->flags & ~HTTP_TRANSFER_ENCODING_MASK) |
+        (transfer_encoding & HTTP_TRANSFER_ENCODING_MASK);
+}
+
+http_content_encoding_t http_get_content_encoding(
+    const http_request_headers_t* const request_headers
+) {
+    return request_headers->flags & HTTP_CONTENT_ENCODING_MASK;
+}
+
+void http_set_content_encoding(
+    http_request_headers_t* const request_headers,
+    const http_content_encoding_t content_encoding
+) {
+    request_headers->flags =
+        (request_headers->flags & ~HTTP_CONTENT_ENCODING_MASK) |
+        (content_encoding & HTTP_CONTENT_ENCODING_MASK);
+}
+
+http_connection_type_t http_get_connection_type(
+    const http_request_headers_t* const request_headers
+) {
+    return request_headers->flags & HTTP_CONNECTION_MASK;
+}
+
+void http_set_connection_type(
+    http_request_headers_t* const request_headers,
+    const http_connection_type_t connection_type
+) {
+    request_headers->flags =
+        (request_headers->flags & ~HTTP_CONNECTION_MASK) |
+        (connection_type & HTTP_CONNECTION_MASK);
+}
+
+bool_t http_is_client_support_encoding(
+    const http_request_headers_t* const request_headers,
+    const http_accept_encoding_t accept_encoding
+) {
+    return (request_headers->flags & accept_encoding) != 0;
+}
+
+void http_set_client_supported_encoding(
+    http_request_headers_t* const request_headers,
+    const http_accept_encoding_t accept_encoding,
+    const bool_t value
+) {
+    if (value)
+        request_headers->flags |= accept_encoding;
+    else
+        request_headers->flags &= ~accept_encoding;
+}
+
+void http_reset_client_supported_encoding(
+    http_request_headers_t* const request_headers
+) {
+    request_headers->flags &= ~HTTP_ACCEPT_ENCODING_MASK;
 }
--- src/http.h
+++ src/http.h
@@ -1,8 +1,131 @@
 #ifndef HTTP_H
 #define HTTP_H
 
+#include "bool.h"
+#include "socket.h"
+
 #include <stdint.h>
 
+#define HTTP_METHOD_SHIFT             0x0u
+#define HTTP_TRANSFER_ENCODING_SHIFT  0x3u
+#define HTTP_CONTENT_ENCODING_SHIFT   0x4u
+#define HTTP_ACCEPT_ENCODING_SHIFT    0x6u
+#define HTTP_CONNECTION_SHIFT         0x9u
+
+#define HTTP_METHOD_MASK             (0x7u << HTTP_METHOD_SHIFT)
+#define HTTP_TRANSFER_ENCODING_MASK  (0x1u << HTTP_TRANSFER_ENCODING_SHIFT)
+#define HTTP_CONTENT_ENCODING_MASK   (0x3u << HTTP_CONTENT_ENCODING_SHIFT)
+#define HTTP_ACCEPT_ENCODING_MASK    (0x7u << HTTP_ACCEPT_ENCODING_SHIFT)
+#define HTTP_CONNECTION_MASK         (0x1u << HTTP_CONNECTION_SHIFT)
+
+typedef enum http_request_method {
+    HTTP_METHOD_NONE    = 0x0u << HTTP_METHOD_SHIFT,
+    HTTP_METHOD_GET     = 0x1u << HTTP_METHOD_SHIFT,
+    HTTP_METHOD_HEAD    = 0x2u << HTTP_METHOD_SHIFT,
+    HTTP_METHOD_POST    = 0x3u << HTTP_METHOD_SHIFT,
+    HTTP_METHOD_PUT     = 0x4u << HTTP_METHOD_SHIFT,
+    HTTP_METHOD_PATCH   = 0x5u << HTTP_METHOD_SHIFT,
+    HTTP_METHOD_DELETE  = 0x6u << HTTP_METHOD_SHIFT,
+    HTTP_METHOD_OPTIONS = 0x7u << HTTP_METHOD_SHIFT
+} http_request_method_t;
+
+typedef enum http_transfer_encoding {
+    HTTP_TRANSFER_ENCODING_NONE    = 0x0u << HTTP_TRANSFER_ENCODING_SHIFT,
+    HTTP_TRANSFER_ENCODING_CHUNKED = 0x1u << HTTP_TRANSFER_ENCODING_SHIFT
+} http_transfer_encoding_t;
+
+typedef enum http_content_encoding {
+    HTTP_CONTENT_ENCODING_NONE    = 0x0u << HTTP_CONTENT_ENCODING_SHIFT,
+    HTTP_CONTENT_ENCODING_ZSTD    = 0x1u << HTTP_CONTENT_ENCODING_SHIFT,
+    HTTP_CONTENT_ENCODING_BR      = 0x2u << HTTP_CONTENT_ENCODING_SHIFT,
+    HTTP_CONTENT_ENCODING_GZIP    = 0x3u << HTTP_CONTENT_ENCODING_SHIFT
+} http_content_encoding_t;
+
+typedef enum http_accept_encoding {
+    HTTP_ACCEPT_ENCODING_ZSTD = 0x1u << (HTTP_ACCEPT_ENCODING_SHIFT + 0x0u),
+    HTTP_ACCEPT_ENCODING_BR   = 0x1u << (HTTP_ACCEPT_ENCODING_SHIFT + 0x1u),
+    HTTP_ACCEPT_ENCODING_GZIP = 0x1u << (HTTP_ACCEPT_ENCODING_SHIFT + 0x2u)
+} http_accept_encoding_t;
+
+typedef enum http_connection_type {
+    HTTP_CONNECTION_CLOSE     = 0x0u << HTTP_CONNECTION_SHIFT,
+    HTTP_CONNECTION_KEEPALIVE = 0x1u << HTTP_CONNECTION_SHIFT
+} http_connection_type_t;
+
+/**
+ * Request headers struct
+ */
+typedef struct http_request_headers {
+    char* uri;
+    int64_t content_length;
+    char* range;
+
+    /**
+     * 3bits: http method (GET|POST|etc.)
+     * 1bit:  Transfer-Encoding (none/chunked)
+     * 2bits: Content-Encoding (none|zstd|br|gzip)
+     * 3bits: Accept-Encoding (gzip, br, zstd)
+     * 1bit:  Connection (keep-alive|close)
+     */
+    uint16_t flags;
+} http_request_headers_t;
+
 int32_t http_handle_request(char* buffer, int32_t size);
+
+int32_t http_send_default_response(socket_fd_t socket_fd,
+                                   char* buffer, int32_t buffer_size,
+                                   int32_t status_code,
+                                   const char* status_description);
+
+http_request_method_t http_get_request_method(
+    const http_request_headers_t* request_headers
+);
+
+void http_set_request_method(
+    http_request_headers_t* request_headers,
+    http_request_method_t request_method
+);
+
+http_transfer_encoding_t http_get_transfer_encoding(
+    const http_request_headers_t* request_headers
+);
+
+void http_set_transfer_encoding(
+    http_request_headers_t* request_headers,
+    http_transfer_encoding_t transfer_encoding
+);
+
+http_content_encoding_t http_get_content_encoding(
+    const http_request_headers_t* request_headers
+);
+
+void http_set_content_encoding(
+    http_request_headers_t* request_headers,
+    http_content_encoding_t content_encoding
+);
+
+http_connection_type_t http_get_connection_type(
+    const http_request_headers_t* request_headers
+);
+
+void http_set_connection_type(
+    http_request_headers_t* request_headers,
+    http_connection_type_t connection_type
+);
+
+bool_t http_is_client_support_encoding(
+    const http_request_headers_t* request_headers,
+    http_accept_encoding_t accept_encoding
+);
+
+void http_set_client_supported_encoding(
+    http_request_headers_t* request_headers,
+    http_accept_encoding_t accept_encoding,
+    bool_t value
+);
+
+void http_reset_client_supported_encoding(
+    http_request_headers_t* request_headers
+);
 
 #endif
